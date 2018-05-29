ab8621bff766d3b22ef0e658ea16a6f690fa23a8
Bearthethird <baloothebear@openmailbox.org>
2018-05-29 22:06:04 +0300

    test commit

--- app/src/main/java/feltech/strixdroidshooter/constructs/domain/ASICAM/ASIcamPreset.java
+++ app/src/main/java/feltech/strixdroidshooter/constructs/domain/ASICAM/ASIcamPreset.java
@@ -3,5 +3,5 @@ package feltech.strixdroidshooter.constructs.domain.ASICAM;
 import feltech.strixdroidshooter.constructs.domain.AbstractCameraPreset;
 
 public class ASIcamPreset extends AbstractCameraPreset {
-
+    int i = 0;
 }
